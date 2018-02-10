40b6d2bea4a1bee2a772ad71bd306aea5baed0aa
NAGA SAHITHYA PAPPU <sahi@MacBook-Pro.local>
2018-02-10 11:12:55 -0800

    other files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <span uk-icon="icon: check"></span>
+</template>
+
+<script>
+import Toolbar from './Toolbar'
+export default {
+  components: {
+    Toolbar
+  }
+}
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="toolbar">
+    <i @click="addNote" uk-icon="icon: plus"></i>
+    <i @click="toggleFavorite"
+      uk-icon="icon: star"
+      :class="{starred: activeNote.favorite}"></i>
+    <i @click="deleteNote" uk-icon="icon: minus"></i>
+  </div>
+</template>
+
+<script>
+import { addNote, deleteNote, toggleFavorite } from '../vuex/actions'
+export default {
+  vuex: {
+    getters: {
+      activeNote: state => state.activeNote
+    },
+    actions: {
+      addNote,
+      deleteNote,
+      toggleFavorite
+    }
+  }
+}
+</script>
